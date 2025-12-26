4e10897b402c67a6da080452a0ccf71dddca0f21
vidyavathi-wow <vidyavathi.telugu@wowlaz.com>
2025-12-26 14:30:44 +0530

    chore: made some fixes

--- src/validators/activity.validators.ts
+++
@@ -1,9 +0,0 @@
-import { body } from "express-validator";
-
-export const activityValidators = {
-  create: [
-    body("action").notEmpty(),
-    body("module").notEmpty(),
-    body("metadata").optional().isObject()
-  ]
-};
